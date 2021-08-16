f53af333c4c9ab2d1996b1245e70d9c3a846ef79
bioerrorlog <n.iijima.0x01@gmail.com>
2021-08-16 20:57:00 +0900

    test passed

--- servo_gpio/servo.py
+++ servo_gpio/servo.py
@@ -4,3 +4,9 @@ from gpiozero import AngularServo
 class Servo:
     def __init__(self, gpio_num):
         self.servo = AngularServo(gpio_num, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+
+    def set_angle(self, angle):
+        self.servo.angle = int(angle)
+
+    def get_angle(self):
+        return self.servo.angle
