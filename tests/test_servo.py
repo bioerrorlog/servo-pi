f1f34e16ced372d9947ad405a7a659dc56617297
bioerrorlog <n.iijima.0x01@gmail.com>
2021-08-18 00:12:12 +0900

    pin number parametrize

--- tests/test_servo.py
+++ tests/test_servo.py
@@ -1,9 +1,11 @@
+import pytest
 from servo_gpio.servo import Servo
 
 
-def test_get_servo_angle(monkeypatch):
+@pytest.mark.parametrize('pin_num', [17, 18, 27])
+def test_get_servo_angle(monkeypatch, pin_num):
     monkeypatch.setenv('GPIOZERO_PIN_FACTORY', 'mock')
     monkeypatch.setenv('GPIOZERO_MOCK_PIN_CLASS', 'mockpwmpin')
-    servo = Servo(18)
+    servo = Servo(pin_num)
     servo.set_angle(60)
     assert servo.get_angle() == 60
