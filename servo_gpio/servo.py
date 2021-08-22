40d0b8e2efc461609e9e832571db4f2c3f4890a7
bioerrorlog <n.iijima.0x01@gmail.com>
2021-08-22 20:48:49 +0900

    Add wavelength as start_wave() args

--- servo_gpio/servo.py
+++ servo_gpio/servo.py
@@ -6,9 +6,9 @@ class SnakeServo(AngularServo):
     def __init__(self, pin=None, *, initial_angle=0.0, min_angle=-90,
                  max_angle=90, min_pulse_width=1/1000, max_pulse_width=2/1000,
                  frame_width=20/1000, pin_factory=None,
-                 wavelength=0.0):
-        self.wavelength = wavelength
-        self.followers = []
+                 wavelength=0.2):
+        self._wavelength = wavelength
+        self._followers = []
         super().__init__(pin, initial_angle=initial_angle,
                          min_angle=min_angle, max_angle=max_angle,
                          min_pulse_width=min_pulse_width,
@@ -16,10 +16,11 @@ class SnakeServo(AngularServo):
                          frame_width=frame_width, pin_factory=pin_factory)
 
     def add_follower(self, follower):
-        self.followers.append(follower)
+        self._followers.append(follower)
 
-    def start_wave(self, angle):
+    def start_wave(self, angle, wavelength=0.2):
         self.angle = angle
-        time.sleep(self.wavelength)
-        for servo in self.followers:
-            servo.start_wave(angle)
+        self._wavelength = wavelength
+        time.sleep(self._wavelength)
+        for servo in self._followers:
+            servo.start_wave(angle, wavelength)
