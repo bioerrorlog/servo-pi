5b20e35e0f27a9421145a0c7be49c8f0b3e245f4
bioerrorlog <n.iijima.0x01@gmail.com>
2021-08-25 23:46:44 +0900

    pivot: use sin for servo angle control

--- servo_gpio/main.py
+++ servo_gpio/main.py
@@ -1,18 +1,29 @@
+import math
+import time
 from gpiozero import AngularServo
 from servo import SnakeServo
 
 def main():
-    servo_17 = SnakeServo(17, wavelength=0.2)
-    servo_18 = SnakeServo(18, wavelength=0.2)
-    servo_27 = SnakeServo(27, wavelength=0.2)
+    # servo_17 = SnakeServo(17, wavelength=0.2)
+    # servo_18 = SnakeServo(18, wavelength=0.2)
+    # servo_27 = SnakeServo(27, wavelength=0.2)
+    servo_17 = AngularServo(17)
+    servo_18 = AngularServo(18)
+    servo_27 = AngularServo(27)
 
-    servo_17.add_follower(servo_18)
-    servo_18.add_follower(servo_27)
+    # servo_17.add_follower(servo_18)
+    # servo_18.add_follower(servo_27)
 
+    t = 0
+    max_angle = 60
+    gap_angle = 30
     while True:
-        servo_17.start_wave(60)
-        servo_17.start_wave(-60)
+        servo_17.angle = math.sin(math.radians(t % 360)) * max_angle
+        servo_18.angle = math.sin(math.radians((t % 360) + gap_angle)) * max_angle
+        servo_27.angle = math.sin(math.radians((t % 360) + (gap_angle * 2))) * max_angle
 
+        t += 1
+        time.sleep(0.1)
     
 if __name__ == '__main__':
     main()
