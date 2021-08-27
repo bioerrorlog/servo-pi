8450dbaef7bf5484dcd8a32a62ec5e913562589e
bioerrorlog <n.iijima.0x01@gmail.com>
2021-08-27 09:54:46 +0900

    adjust params

--- servo_gpio/main.py
+++ servo_gpio/main.py
@@ -15,8 +15,8 @@ def main():
     # servo_18.add_follower(servo_27)
 
     t = 0
-    max_angle = 60
-    gap_angle = 30
+    max_angle = 90
+    gap_angle = 60
     while True:
         servo_17.angle = math.sin(math.radians(t % 360)) * max_angle
         servo_18.angle = math.sin(math.radians((t % 360) + gap_angle)) * max_angle
