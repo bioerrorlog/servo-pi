5f5bc4a20182233abc1941ecce14d8f3691b9c4b
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-11-15 09:53:31 +0900

    delete dependency between main and servo.py

--- servo_gpio/main.py
+++ servo_gpio/main.py
@@ -1,22 +1,17 @@
 import math
 import time
 from gpiozero import AngularServo
-from servo import SnakeServo
+
 
 def main():
-    # servo_17 = SnakeServo(17, wavelength=0.2)
-    # servo_18 = SnakeServo(18, wavelength=0.2)
-    # servo_27 = SnakeServo(27, wavelength=0.2)
     servo_17 = AngularServo(17)
     servo_18 = AngularServo(18)
     servo_27 = AngularServo(27)
 
-    # servo_17.add_follower(servo_18)
-    # servo_18.add_follower(servo_27)
-
     t = 0
     max_angle = 60
     gap_angle = 30
+
     try:
         while True:
             servo_17.angle = math.sin(math.radians(t % 360)) * max_angle
@@ -30,6 +25,6 @@ def main():
         servo_18.angle = 0
         servo_27.angle = 0
 
-    
+
 if __name__ == '__main__':
-    main() 
+    main()
